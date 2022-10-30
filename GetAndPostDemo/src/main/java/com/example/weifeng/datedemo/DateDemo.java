350db9c550acfc46c0ba1326c8ee21613dcea8a5
weifeng_wu <wmy_weifeng@163.com>
2022-10-30 23:04:38 +0800

    整理了一下mock

--- GetAndPostDemo/src/main/java/com/example/weifeng/datedemo/DateDemo.java
+++ GetAndPostDemo/src/main/java/com/example/weifeng/datedemo/DateDemo.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class a {
+public class DateDemo {
 
     public static void main(String[] args) {
         BigDecimal a = new BigDecimal(0.22333);
